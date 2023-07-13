5acd845a1df399ebe294d5777fe148b222b7a345
lentrix <hawkmanlentrix@gmail.com>
2023-07-13 08:55:32 +0800

    before email

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -13,6 +13,39 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::factory(30)->create();
+        Client::factory(20)->create();
+        $cl = [
+            [
+                'last_name' => 'Lenteria',
+                'first_name' => 'Benjie',
+                'middle_name' => 'Basio',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'benjielenteria@yahoo.com'
+            ],
+            [
+                'last_name' => 'Lentrix',
+                'first_name' => 'Hawkman',
+                'middle_name' => 'Machiacode',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'hawkmanlentrix@gmail.com'
+            ],
+            [
+                'last_name' => 'Iphitech',
+                'first_name' => 'Bench',
+                'middle_name' => 'Developer',
+                'address' => 'Pob. Centro, Clarin, Bohol',
+                'birth_date' => '1979-10-02',
+                'phone_number' => '09090892350',
+                'email' => 'iphitech.lenteria.benjie@gmail.com'
+            ],
+        ];
+
+        foreach($cl as $c) {
+            Client::create($c);
+        }
     }
 }
